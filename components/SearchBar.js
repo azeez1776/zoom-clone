f773dd92e83a788a039013dbae88179525bc5b32
azeez1776 <aboud3am81@gmail.com>
2021-10-04 02:42:06 +0300

    styled the SearchBar.js

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import Fontisto from 'react-native-vector-icons/Fontisto'
 
 const SearchBar = () => {
     return (
-        <View>
-            <Text>Search Bar</Text>
+        <View style={styles.container}>
+            <Fontisto name="search" size={20} color="#858585" />
+            <Text style={styles.textSearchBar}>Search</Text>
         </View>
     )
 }
 
-export default SearchBar
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#333333',
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+        height: 40,
+        alignItems: 'center',
+        borderRadius: 10
+    },
+    textSearchBar: {
+        color: '#858585',
+        fontSize: 20,
+        paddingLeft: 10
+    }
 
-const styles = StyleSheet.create({})
+})
+
+export default SearchBar
