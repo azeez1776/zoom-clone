6d65b696b8fb19e4a607e8edfed3e4eaf1f25b76
azeez1776 <aboud3am81@gmail.com>
2021-10-04 01:33:16 +0300

    Added Home.js file and imported it to App.js file

---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default Home
