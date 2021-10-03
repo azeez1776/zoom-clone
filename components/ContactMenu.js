d236e0d9a3384a4254a91f109bb124c22ddffa0e
azeez1776 <aboud3am81@gmail.com>
2021-10-04 01:47:52 +0300

    added ContactMenu.js

---
+++ components/ContactMenu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ContactMenu = () => {
+    return (
+        <View>
+            <Text>Contact Menu</Text>
+        </View>
+    )
+}
+
+export default ContactMenu
+
+const styles = StyleSheet.create({})
