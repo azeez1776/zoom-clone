6ed24984d3816f5ab26516265c1d8624acf117de
azeez1776 <aboud3am81@gmail.com>
2021-10-04 02:26:56 +0300

    finished styling header

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Entypo from 'react-native-vector-icons/Entypo'
 
 const Header = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Entypo name="notification" size={30} color="#efefef" />
             <Text style={styles.heading}>Meet & Chat</Text>
             <Entypo name="new-message" size={30} color="#efefef" />
@@ -16,6 +16,13 @@ const Header = () => {
 export default Header
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignContent: 'center',
+        paddingVertical: 20,
+        paddingHorizontal: 10
+    },
     heading: {
         color: '#efefef',
         fontSize: 20,
