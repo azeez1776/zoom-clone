ba9235a2f1243c8bc6b3b56d3beba8bbcf3623b2
azeez1776 <aboud3am81@gmail.com>
2021-10-04 06:34:07 +0300

    styled contact layout

--- components/ContactMenu.js
+++ components/ContactMenu.js
@@ -1,14 +1,41 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const ContactMenu = () => {
     return (
-        <View>
-            <Text>Contact Menu</Text>
+        <View style={styles.container}>
+            <View style={styles.row}>
+                <View style={styles.starredIcon}>
+                    <AntDesign name="star" size={23} color="#efefef" />
+                </View>
+                <Text style={styles.text}>
+                    Starred
+                </Text>
+            </View>
         </View>
     )
 }
 
-export default ContactMenu
+const styles = StyleSheet.create({
+    starredIcon: {
+        backgroundColor: '#333333',
+        width: 55,
+        height: 55,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20
+    },
+    row: {
+        flexDirection: 'row',
+        marginTop: 20,
+        alignItems: 'center'
+    },
+    text: {
+        color: 'white',
+        paddingLeft: 15,
+        fontSize: 15
+    }
+})
 
-const styles = StyleSheet.create({})
+export default ContactMenu
