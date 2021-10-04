ccb3703f55f78b7c6956a0456094e10088cbb227
azeez1776 <aboud3am81@gmail.com>
2021-10-05 01:47:36 +0300

    made small changes

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { createStackNavigation } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
 
 const MeetingRoom = () => {
     return (
