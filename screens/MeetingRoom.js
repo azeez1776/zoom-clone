1c3c993d792e7650cc966696eae9c3dc63e3d50f
azeez1776 <aboud3am81@gmail.com>
2021-10-05 01:24:45 +0300

    Added MeetingRoom.js file

---
+++ screens/MeetingRoom.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const MeetingRoom = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default MeetingRoom
+
+const styles = StyleSheet.create({})
