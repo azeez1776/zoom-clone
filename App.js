9bde92a81e4a045cc90720d4fc31fb1e1db88394
azeez1776 <aboud3am81@gmail.com>
2021-10-08 01:26:57 +0300

    added Navigation component

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home';
+import Navigation from './Navigation';
 
 export default function App() {
-  return <Home />
+  return <Navigation />
 }
 
 const styles = StyleSheet.create({
