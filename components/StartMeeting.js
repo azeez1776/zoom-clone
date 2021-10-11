3515f9b197d4ac3a0c705bf6f9ffcc744035530a
azeez1776 <aboud3am81@gmail.com>
2021-10-11 16:54:18 +0300

    imported TouchableOpacity and TextInput

--- components/StartMeeting.js
+++ components/StartMeeting.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 
 const StartMeeting = ({ name, roomID, setName, setRoomID }) => {
     return (
