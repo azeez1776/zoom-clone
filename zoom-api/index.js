2d53f087ff22ee5c3ff63ac6098b946b026f3c3e
azeez1776 <aboud3am81@gmail.com>
2021-10-11 16:40:43 +0300

    implemented socket.io durinfg connection

--- zoom-api/index.js
+++ zoom-api/index.js
@@ -6,9 +6,18 @@ const port = 3001;
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+});
+
+io.on("connection", socket => {
+    console.log('Someone connected');
+    socket.on('join-room', (roomID, userName) => {
+        console.log('User joined room');
+        console.log(roomID);
+        console.log(userName);
+    })
 })
 
 
-server.listen(port => {
+server.listen(port, () => {
     console.log(`Listening to port ${port}`)
 })
