7bc35e3c00b238d2cb2a6efb781b45fc1ce27f9e
azeez1776 <aboud3am81@gmail.com>
2021-10-11 16:17:22 +0300

    made the get http request and listening to port 3001

--- zoom-api/index.js
+++ zoom-api/index.js
@@ -2,3 +2,13 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+
+server.listen(port => {
+    console.log(`Listening to port ${port}`)
+})
