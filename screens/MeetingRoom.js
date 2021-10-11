93c9a08fec431c868bb79729b65a992560383b3c
azeez1776 <aboud3am81@gmail.com>
2021-10-11 17:48:52 +0300

    added joinRoom function

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { io } from 'socket.io-client';
 import StartMeeting from '../components/StartMeeting';
 
 
@@ -8,8 +9,16 @@ const MeetingRoom = () => {
     const [name, setName] = useState('');
     const [roomID, setRoomID] = useState('');
 
-    const startMeeting = () => {
+    useEffect(() => {
+        const API_URL = 'http://localhost:3001/';
+        const socket = io(API_URL);
+        socket.on('connection', () => {
+            console.log('connected');
+        })
+    }, [])
 
+    const joinRoom = () => {
+        socket.emit('join-room', { roomID: roomID, userName: name })
     }
 
     return (
