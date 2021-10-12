b60ca4a73e8d19d8160b977bdaba98dae0a269db
azeez1776 <aboud3am81@gmail.com>
2021-10-12 13:14:18 +0300

    Added the camera option using expo-camera

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native'
 import { io } from 'socket.io-client';
 import StartMeeting from '../components/StartMeeting';
 import { Camera } from 'expo-camera';
@@ -12,13 +12,22 @@ const MeetingRoom = () => {
     const [name, setName] = useState('');
     const [roomID, setRoomID] = useState('');
     const [activeUsers, setActiveUsers] = useState();
+    const [startCamera, setStartCamera] = useState(false);
 
     const joinRoom = () => {
+        switchCamera();
+        setStartCamera(true)
         socket.emit('join-room', { roomID: roomID, userName: name })
     }
 
+    const switchCamera = async () => {
+        const { status } = await Camera.requestPermissionsAsync();
+        if (status === 'granted') setStartCamera(true);
+        else Alert.alert('Access Denied');
+    }
+
     useEffect(() => {
-        const API_URL = 'http://c250-196-249-97-238.ngrok.io/';
+        const API_URL = 'http://996e-196-249-98-53.ngrok.io/';
         socket = io(API_URL);
         socket.on('connection', () => {
             console.log('connected');
@@ -32,16 +41,28 @@ const MeetingRoom = () => {
 
     return (
         <View style={styles.container}>
-            <StartMeeting
-                name={name}
-                roomID={roomID}
-                setName={setName}
-                setRoomID={setRoomID}
-                joinRoom={joinRoom}
-            />
+            {startCamera ? (
+                <View>
+                    <Camera
+                        type="front"
+                        style={{ width: '100%', height: 600 }}
+                    />
+                </View>
+            ) :
+                (
+                    <StartMeeting
+                        name={name}
+                        roomID={roomID}
+                        setName={setName}
+                        setRoomID={setRoomID}
+                        joinRoom={joinRoom}
+                    />
+                )
+            }
         </View>
     )
 }
+
 
 const styles = StyleSheet.create({
     container: {
