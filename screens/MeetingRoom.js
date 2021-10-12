8fbd12f0f749661687ddd0286725ee702414fa6b
azeez1776 <aboud3am81@gmail.com>
2021-10-12 17:36:12 +0300

    styled the camera view for users invited

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,17 +1,41 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, SafeAreaView } from 'react-native'
 import { io } from 'socket.io-client';
 import StartMeeting from '../components/StartMeeting';
 import { Camera } from 'expo-camera';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 let socket;
+const menuItems = [
+    {
+        id: 1,
+        name: 'microphone',
+        text: 'Mute'
+    },
+    {
+        id: 2,
+        name: 'video-camera',
+        text: 'Stop Video'
+    },
+    {
+        id: 3,
+        name: 'upload',
+        text: 'Share Content'
+    },
+    {
+        id: 4,
+        name: 'group',
+        text: 'Participants'
+    },
+]
+
 
 const MeetingRoom = () => {
 
 
     const [name, setName] = useState('');
     const [roomID, setRoomID] = useState('');
-    const [activeUsers, setActiveUsers] = useState();
+    const [activeUsers, setActiveUsers] = useState(['mariki', 'tony']);
     const [startCamera, setStartCamera] = useState(false);
 
     const joinRoom = () => {
@@ -42,12 +66,43 @@ const MeetingRoom = () => {
     return (
         <View style={styles.container}>
             {startCamera ? (
-                <View>
-                    <Camera
-                        type="front"
-                        style={{ width: '100%', height: 600 }}
-                    />
-                </View>
+                <SafeAreaView style={{ flex: 1 }}>
+                    <View style={styles.master}>
+                        <View style={styles.mainView}>
+                            <View style={styles.cameraContainer}>
+                                <Camera
+                                    type="front"
+                                    style={{
+                                        width: activeUsers === 0 ? '100%' : 200,
+                                        height: activeUsers === 0 ? 600 : 200
+                                    }}
+                                />
+                            </View>
+                            <View style={styles.userInvite}>
+                                {activeUsers.map((user, id) => {
+                                    return (
+                                        <View style={styles.userView} key={id}>
+                                            <Text style={styles.textUser}>{user}</Text>
+                                        </View>
+                                    )
+                                })}
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.menu} >
+                        {menuItems.map((icon, id) => {
+                            return (
+                                <TouchableOpacity
+                                    style={styles.tile}
+                                    key={id}
+                                >
+                                    <FontAwesome name={icon.name} size={24} color={"#efefef"} />
+                                    <Text style={styles.textTile}>{icon.text}</Text>
+                                </TouchableOpacity>
+                            )
+                        })}
+                    </View>
+                </SafeAreaView>
             ) :
                 (
                     <StartMeeting
@@ -69,6 +124,53 @@ const styles = StyleSheet.create({
         backgroundColor: '#1c1c1c',
         flex: 1
     },
+    tile: {
+        alignItems: 'center',
+        margin: 5
+    },
+    textTile: {
+        color: 'white',
+        marginTop: 10
+    },
+    menu: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    mainView: {
+        flex: 1,
+        justifyContent: 'center',
+        // backgroundColor: 'black',
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        width: '100%'
+    },
+    textUser: {
+        color: 'white'
+    },
+    userView: {
+        height: 200,
+        width: 200,
+        backgroundColor: 'grey',
+        borderWidth: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    userInvite: {
+        flexDirection: 'row',
+        // flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    master: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        backgroundColor: 'black'
+    }
 
 
 })
